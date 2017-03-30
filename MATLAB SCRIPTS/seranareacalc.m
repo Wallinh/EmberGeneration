330d0dab4ae91cb5fd3b6aef7d98d8ae8c95ce79
Wallinh <wallinh@users.noreply.github.com>
2017-03-30 16:50:05 -0700

    update comments, add area calc, structure format

--- MATLAB SCRIPTS/seranareacalc.m
+++ MATLAB SCRIPTS/seranareacalc.m
@@ -2,20 +2,18 @@ clc
 clear
 close all
 
-%% script to calculate area and number of holes in seran wrap
+%% script to calculate number of holes in seran wrap
 
 %% Set  calibration
 % this will be done once per picture set
-% as instructed, set crop and scale calibration for photo
-% set
+% as instructed, set crop and scale calibration for photo set
 
-cd ..\Photos\Test   %modify for folder of interest
+cd ..\Photos\Test   %modify to folder of photos
 contents = dir('*.jpg');
 
-filename = contents(1).name;
+filename = contents(4).name; %make this index of photo to set crop and scale
 
-
-img = imread(filename); %make this first file of photos
+img = imread(filename); 
 imshow(img)
 
 
@@ -38,10 +36,11 @@ Cal_scale  = Cal_AREA/Cal_pixels;                              % mm^2/pixel
 
 %% image processing
 
-%to process the image we will  resize the image, then equalize the
-%histogram to prodive stronger contrast. Once this is complete convert to
-%grayscale, then convert to Bianary with adaptive or global thresholding.
+%to process the image we will  resize the image,
+%convert to grayscale, then convert to Bianary with adaptive or global thresholding.
 
+%for the trashbag case, histogram equalization is not needed, nor is
+%adaptive thresholding.
 
 for i = 1:numel(contents)
     
@@ -62,10 +61,6 @@ for i = 1:numel(contents)
     histOriginal = imhist(imgGs);
     imgGsEq = histeq(imgGs);
     
-    %display images
-%     imshow(imgGs),title('original')
-%     figure, imshow(imgGsEq),title('adjsuted')
-    
     %display hists
 %     figure, imhist(imgGs) ,title('original')
 %     figure,imhist(imgGsEq) ,title('adjsuted')
@@ -73,47 +68,54 @@ for i = 1:numel(contents)
     % sutract background from image to elimitate uneven background illumination
     %https://www.mathworks.com/help/images/image-enhancement-and-analysis.html
     
-    
-    
     %binarize grayscale image
     
-     imgGs = imcomplement(imgGs) ;  %this inverts image if needed. delete if not needed.
+  
     %
-    %         %method one ADAPTIVE
+    %         %METHOD ! ADAPTIVE
     %     imgBiAdaptive = imbinarize(imgGs,'adaptive','Sensitivity',0.5);       %sensitivity is .5 by default; %'ForegroundPolarity','dark'
     %     figure
     %     imshowpair(imgGsEq,imgBiAdaptive,'montage'), title ('adaptive');
     %
-    %method 2, global
+    
+    
+    %METHOD 2, global
     %invert image
-    imgBiGlobal = imbinarize(imgGs,'global') ;% threshold can be changed with graythresh function
+    imgGs = imcomplement(imgGs);
+    imgBiGlobal = imbinarize(imgGs,'global') ;
     figure
     imshowpair(imgGs,imgBiGlobal,'montage'), title ('global');
     
-    %display the changes
-    
-    
-    
-    
+  
     %% image calculations
     
-    %object anaysis
+    %OBJECT ANALYSIS
+    %https://www.mathworks.com/help/images/pixel-values-and-image-statistics.html
     %https://www.mathworks.com/help/images/object-analysis.html
     
-    stats = regionprops(imgBiGlobal,'area');
+    %create a structure, where each image is an group. 
+    %this is done two ways, need to verify which is appropriate test. 
+    %bw area:  The area of an individual pixel is determined by looking at its 2-by-2 neighborhood
+    infostruc(i).pixels =  regionprops(imgBiGlobal,'area');
+    infostruc(i).bwarea = bwarea(imBiGlobal)
     
-    [B,L] = bwboundaries(imgBiGlobal,'noholes');
-    imshow(label2rgb(L,@jet,[.5 .5 .5]))
-    hold on
-    for k = 1:length(B)
-        boundary = B{k};
-        plot(boundary(:,2),boundary(:,1),'w','lineWidth',2)
-    end
-    
+
+
+ %this traces boundaries on holes. Not necessary for this project. 
+ 
+ 
+%     [B,L] = bwboundaries(imgBiGlobal,'noholes');
+%     imshow(label2rgb(L,@jet,[.5 .5 .5]))
+%     hold on
+%     for k = 1:length(B)
+%         boundary = B{k};
+%         plot(boundary(:,2),boundary(:,1),'w','lineWidth',2)
+%     end
+%     
     
 end
-%region properties
-%https://www.mathworks.com/help/images/pixel-values-and-image-statistics.html
+
+
 
 
 
